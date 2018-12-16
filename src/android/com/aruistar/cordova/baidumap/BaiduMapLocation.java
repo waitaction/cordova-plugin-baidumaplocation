c570a859de95869e2c09b097e9992ebe529a453d
chenqijing <qijing@wo.cn>
2018-12-16 22:22:21 +0800

    测试

--- src/android/com/aruistar/cordova/baidumap/BaiduMapLocation.java
+++ src/android/com/aruistar/cordova/baidumap/BaiduMapLocation.java
@@ -1,6 +1,5 @@
 package com.aruistar.cordova.baidumap;
 
-
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Build;
@@ -86,7 +85,6 @@ public class BaiduMapLocation extends CordovaPlugin {
                     pluginResult = new PluginResult(PluginResult.Status.OK, json);
                 }
 
-
                 cbCtx.sendPluginResult(pluginResult);
             } catch (JSONException e) {
                 String errMsg = e.getMessage();
@@ -109,7 +107,8 @@ public class BaiduMapLocation extends CordovaPlugin {
 
     private boolean needsToAlertForRuntimePermission() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            return !cordova.hasPermission(Manifest.permission.ACCESS_COARSE_LOCATION) || !cordova.hasPermission(Manifest.permission.ACCESS_FINE_LOCATION);
+            return !cordova.hasPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
+                    || !cordova.hasPermission(Manifest.permission.ACCESS_FINE_LOCATION);
         } else {
             return false;
         }
@@ -129,7 +128,8 @@ public class BaiduMapLocation extends CordovaPlugin {
         cordova.requestPermissions(this, REQUEST_CODE, _permissionsToRequire);
     }
 
-    public void onRequestPermissionResult(int requestCode, String[] permissions, int[] grantResults) throws JSONException {
+    public void onRequestPermissionResult(int requestCode, String[] permissions, int[] grantResults)
+            throws JSONException {
         if (cbCtx == null || requestCode != REQUEST_CODE)
             return;
         for (int r : grantResults) {
@@ -152,18 +152,20 @@ public class BaiduMapLocation extends CordovaPlugin {
     public boolean execute(String action, final JSONArray args, CallbackContext callbackContext) throws JSONException {
         cbCtx = callbackContext;
         if ("getCurrentPosition".equalsIgnoreCase(action)) {
-            if (!needsToAlertForRuntimePermission()) {
-                performGetLocation();
-            } else {
-                requestPermission();
-                // 会在onRequestPermissionResult时performGetLocation
-            }
+            // if (!needsToAlertForRuntimePermission()) {
+            // performGetLocation();
+            // } else {
+            // requestPermission();
+            // // 会在onRequestPermissionResult时performGetLocation
+            // }
+            requestPermission();
+            performGetLocation();
+
             return true;
         }
 
         return false;
     }
-
 
     /**
      * 权限获得完毕后进行定位
@@ -178,23 +180,22 @@ public class BaiduMapLocation extends CordovaPlugin {
         mLocationClient.start();
     }
 
-
     public LocationClientOption getDefaultLocationClientOption() {
         if (mOption == null) {
             mOption = new LocationClientOption();
-            mOption.setLocationMode(LocationMode.Hight_Accuracy);//可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
-            mOption.setCoorType("bd09ll");//可选，默认gcj02，设置返回的定位结果坐标系，如果配合百度地图使用，建议设置为bd09ll;
-            mOption.setScanSpan(0);//可选，默认0，即仅定位一次，设置发起定位请求的间隔需要大于等于1000ms才是有效的
-            mOption.setIsNeedAddress(true);//可选，设置是否需要地址信息，默认不需要
-            mOption.setOpenGps(true); // 可选，默认false,设置是否使用gps            
-            mOption.setNeedDeviceDirect(false);//可选，设置是否需要设备方向结果
-            mOption.setLocationNotify(false);//可选，默认false，设置是否当gps有效时按照1S1次频率输出GPS结果
-            mOption.setIgnoreKillProcess(true);//可选，默认true，定位SDK内部是一个SERVICE，并放到了独立进程，设置是否在stop的时候杀死这个进程，默认不杀死
-            mOption.setIsNeedLocationDescribe(true);//可选，默认false，设置是否需要位置语义化结果，可以在BDLocation.getLocationDescribe里得到，结果类似于“在北京天安门附近”
-            mOption.setIsNeedLocationPoiList(false);//可选，默认false，设置是否需要POI结果，可以在BDLocation.getPoiList里得到
-            mOption.SetIgnoreCacheException(false);//可选，默认false，设置是否收集CRASH信息，默认收集
+            mOption.setLocationMode(LocationMode.Hight_Accuracy);// 可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
+            mOption.setCoorType("bd09ll");// 可选，默认gcj02，设置返回的定位结果坐标系，如果配合百度地图使用，建议设置为bd09ll;
+            mOption.setScanSpan(0);// 可选，默认0，即仅定位一次，设置发起定位请求的间隔需要大于等于1000ms才是有效的
+            mOption.setIsNeedAddress(true);// 可选，设置是否需要地址信息，默认不需要
+            mOption.setOpenGps(true); // 可选，默认false,设置是否使用gps
+            mOption.setNeedDeviceDirect(false);// 可选，设置是否需要设备方向结果
+            mOption.setLocationNotify(false);// 可选，默认false，设置是否当gps有效时按照1S1次频率输出GPS结果
+            mOption.setIgnoreKillProcess(true);// 可选，默认true，定位SDK内部是一个SERVICE，并放到了独立进程，设置是否在stop的时候杀死这个进程，默认不杀死
+            mOption.setIsNeedLocationDescribe(true);// 可选，默认false，设置是否需要位置语义化结果，可以在BDLocation.getLocationDescribe里得到，结果类似于“在北京天安门附近”
+            mOption.setIsNeedLocationPoiList(false);// 可选，默认false，设置是否需要POI结果，可以在BDLocation.getPoiList里得到
+            mOption.SetIgnoreCacheException(false);// 可选，默认false，设置是否收集CRASH信息，默认收集
 
-            mOption.setIsNeedAltitude(false);//可选，默认false，设置定位时是否需要海拔信息，默认不需要，除基础定位版本都可用
+            mOption.setIsNeedAltitude(false);// 可选，默认false，设置定位时是否需要海拔信息，默认不需要，除基础定位版本都可用
 
         }
         return mOption;
